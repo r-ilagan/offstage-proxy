df6f72fbfd6b09fb80d6f5da3ec22c3f364b4da0
Rach Ilagan <cachola.matthew@gmail.com>
2021-01-07 21:22:25 -0500

    feat: Add tmdb route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import middleware from './middleware';
-import tmdb from './api/tmdb';
+import api from './api';
+import dotenv from 'dotenv';
 const morgan = require('morgan'); // removes deprecated warning caused by esm
+
+dotenv.config();
 
 const app = express();
 
@@ -10,7 +13,13 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(json());
 
-app.use('/api/v1/tmdb', tmdb);
+app.get('/', (req, res) => {
+  res.json({
+    message: '👋 Hello there!',
+  });
+});
+
+app.use('/api/v1', api);
 
 app.use(middleware.notFound);
 app.use(middleware.errorHandler);
